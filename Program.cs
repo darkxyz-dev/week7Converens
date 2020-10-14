13dbc26c0b76dc7728ae01e8296a8676df0aba2c
Opilane TTHK <opilane@tthk.ee>
2020-10-14 16:11:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace week7Converents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double fahrenheit;
+
+            double celsius = 36;
+            Console.WriteLine("Celsius: " + celsius);
+
+            fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+            Console.ReadLine();
+               }
+    }
+}
